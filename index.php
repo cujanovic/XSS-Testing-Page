468686fa55f4070cc4c39659e481455124d3c5fe
Predrag Cujanovic <predrag@cujanovic.com>
2020-05-07 22:32:44 +0200

    update

--- index.php
+++ index.php
@@ -28,109 +28,109 @@
           <tr>
             <td>tagName</td>
             <td>Inside a tag declaration</td>
-            <td><a href="?tagName=foo">index.php?tagName=foo</a></td>
+            <td><a href="?tagName=foo">?tagName=foo</a></td>
             <td>&lt;foo&gt;...&lt;/foo&gt;</td>
           </tr>
           <tr>
             <td>attributeName</td>
             <td>Inside an attribute name</td>
-            <td><a href="?attributeName=foo">index.php?attributeName=foo</a></td>
+            <td><a href="?attributeName=foo">?attributeName=foo</a></td>
             <td>&lt;div foo=""&gt;...&lt;/div&gt;</td>
           </tr>
           <tr>
             <td>singleQuotedAttributeValue</td>
             <td>Inside an attribute value delimeted by a single quote (<code>'</code>)</td>
-            <td><a href="?singleQuotedAttributeValue=foo">index.php?singleQuotedAttributeValue=foo</a></td>
+            <td><a href="?singleQuotedAttributeValue=foo">?singleQuotedAttributeValue=foo</a></td>
             <td>&lt;input value='foo'&gt;...&lt;/input&gt;</td>
           </tr>
           <tr>
             <td>doubleQuotedAttributeValue</td>
             <td>Inside an attribute value delimeted by a double quote (<code>"</code>)</td>
-            <td><a href="?doubleQuotedAttributeValue=foo">index.php?doubleQuotedAttributeValue=foo</a></td>
+            <td><a href="?doubleQuotedAttributeValue=foo">?doubleQuotedAttributeValue=foo</a></td>
             <td>&lt;input value="foo"&gt;...&lt;/input&gt;</td>
           </tr>
           <tr>
             <td>unquotedAttributeValue</td>
             <td>Inside an attribute value delimeted by a double quote (<code>"</code>)</td>
-            <td><a href="?unquotedAttributeValue=foo">index.php?unquotedAttributeValue=foo</a></td>
+            <td><a href="?unquotedAttributeValue=foo">?unquotedAttributeValue=foo</a></td>
             <td>&lt;input value=foo&gt;...&lt;/input&gt;</td>
           </tr>
           <tr>
             <td>urlencodeuri</td>
             <td>Test if browser sends urlencode URI</td>
-            <td><a href="?urlencodeuri=foo>'<;">index.php?urlencodeuri=foo>'<;</a></td>
+            <td><a href="?urlencodeuri=foo>'<;">?urlencodeuri=foo>'<;</a></td>
             <td>/xss/?urlencodeuri=foo%3E%27%3C;</td>
           </tr>
             <td>html</td>
             <td>In an HTML context</td>
-            <td><a href="?html=foo">index.php?html=foo</a></td>
+            <td><a href="?html=foo">?html=foo</a></td>
             <td>&lt;div&gt;... foo ...&lt;/div&gt;</td>
           </tr>
           <tr>
             <td>html ahref</td>
             <td>In an HTML ahref context</td>
-            <td><a href="?ahref=foo">index.php?ahref=foo</a></td>
+            <td><a href="?ahref=foo">?ahref=foo</a></td>
             <td>&lt;a href=&quot;foo&quot;&gt;... foo ...&lt;/a&gt;</td>
           </tr>
           <tr>
             <td>htmlComment</td>
             <td>Inside an HTML comment</td>
-            <td><a href="?htmlComment=foo">index.php?htmlComment=foo</a></td>
+            <td><a href="?htmlComment=foo">?htmlComment=foo</a></td>
             <td>&lt;!--... foo ... --&gt;</td>
           </tr>
           <tr>
             <td>styleTag</td>
             <td>Inside a CSS style tag</td>
-            <td><a href="?styleTag=foo">index.php?styleTag=foo</a></td>
+            <td><a href="?styleTag=foo">?styleTag=foo</a></td>
             <td>&lt;style&gt;... foo ...&lt;/div&gt;</td>
           </tr>
           <tr>
             <td>styleAttribute</td>
             <td>Inside a CSS style attribute</td>
-            <td><a href="?styleAttribute=foo">index.php?styleAttribute=foo</a></td>
+            <td><a href="?styleAttribute=foo">?styleAttribute=foo</a></td>
             <td>&lt;div style="foo"&gt;...&lt;/div&gt;</td>
           </tr>
           <tr>
             <td>idAttribute</td>
             <td>Inside an ID attribute</td>
-            <td><a href="?idAttribute=foo">index.php?idAttribute=foo</a></td>
+            <td><a href="?idAttribute=foo">?idAttribute=foo</a></td>
             <td>&lt;div id="foo"&gt;...&lt;/div&gt;</td>
           </tr>
           <tr>
             <td>classAttribute</td>
             <td>Inside a CSS class name attribute</td>
-            <td><a href="?classAttribute=foo">index.php?classAttribute=foo</a></td>
+            <td><a href="?classAttribute=foo">?classAttribute=foo</a></td>
             <td>&lt;div class="foo"&gt;...&lt;/div&gt;</td>
           </tr>
 
           <tr>
             <td>jsSingleQuotedString</td>
             <td>Inside a sigle quoted string in JavaScript</td>
-            <td><a href="?jsSingleQuotedString=foo">index.php?jsSingleQuotedString=foo</a></td>
+            <td><a href="?jsSingleQuotedString=foo">?jsSingleQuotedString=foo</a></td>
             <td>&lt;script&gt; var str = 'foo'; &lt;/script&gt;</td>
           </tr>
           <tr>
             <td>jsDoubleQuotedString</td>
             <td>Inside a double quoted string in JavaScript</td>
-            <td><a href="?jsDoubleQuotedString=foo">index.php?jsDoubleQuotedString=foo</a></td>
+            <td><a href="?jsDoubleQuotedString=foo">?jsDoubleQuotedString=foo</a></td>
             <td>&lt;script&gt; var str = "foo"; &lt;/script&gt;</td>
           </tr>
           <tr>
             <td>jsSingleLineComment</td>
             <td>Inside a single line JavaScript comment</td>
-            <td><a href="?jsSingleLineComment=foo">index.php?jsSingleLineComment=foo</a></td>
+            <td><a href="?jsSingleLineComment=foo">?jsSingleLineComment=foo</a></td>
             <td>&lt;script&gt; // foo &lt;/script&gt;</td>
           </tr>
           <tr>
             <td>jsMultiLineComment</td>
             <td>Inside a multi line JavaScript comment</td>
-            <td><a href="?jsMultiLineComment=foo">index.php?jsMultiLineComment=foo</a></td>
+            <td><a href="?jsMultiLineComment=foo">?jsMultiLineComment=foo</a></td>
             <td>&lt;script&gt; /* foo */ &lt;/script&gt;</td>
           </tr>
           <tr>
             <td>js</td>
             <td>Inside a JavaScript script tag context</td>
-            <td><a href="?js=foo">index.php?js=foo</a></td>
+            <td><a href="?js=foo">?js=foo</a></td>
             <td>&lt;script&gt; foo &lt;/script&gt;</td>
           </tr>
         </tbody>
